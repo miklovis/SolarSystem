3ce245342c2d1b84600ebd68e9d3e49b13791bb8
Miklovis <miklovis@egb076000019.lancs.local>
2020-10-13 11:32:56 +0100

    Cleaned up the Driver file of redundant blocks of code, now more easily readable

--- Driver.java
+++ Driver.java
@@ -4,7 +4,19 @@ public class Driver {
     
     public static void main(String[] args)
     {
-        ss = new SolarSystem(1000, 1000);
+        ss = new SolarSystem(1000, 1000);      
+        createPlanets();
+
+        while(true){
+            ss.drawSolarObject(0, 0, 50, "YELLOW");
+            for(int i = 0; i < 8; i++){
+                systemPlanets[i].dayPassed(ss);
+            }
+            ss.finishedDrawing();
+        }
+    }
+
+    public static void createPlanets(){
         systemPlanets[0] = new Planet(ss, 50, 88, 10, "WHITE");
         systemPlanets[1] = new Planet(ss, 90, 225, 20, "YELLOW");
         systemPlanets[2] = new Planet(ss, 140, 365, 25, "BLUE");
@@ -12,21 +24,6 @@ public class Driver {
         systemPlanets[4] = new Planet(ss, 250, 4333, 70, "ORANGE");
         systemPlanets[5] = new Planet(ss, 310, 10759, 100, "YELLOW");
         systemPlanets[6] = new Planet(ss, 380, 30687, 40, "CYAN");
-        systemPlanets[7] = new Planet(ss, 420, 60190, 25, "BLUE");        
-        
-        while(true){
-            System.out.println("Labas");
-            ss.drawSolarObject(0, 0, 50, "YELLOW");
-            systemPlanets[0].dayPassed(ss);
-            systemPlanets[1].dayPassed(ss);
-            systemPlanets[2].dayPassed(ss);
-            systemPlanets[3].dayPassed(ss);
-            systemPlanets[4].dayPassed(ss);
-            systemPlanets[5].dayPassed(ss);
-            systemPlanets[6].dayPassed(ss);
-            systemPlanets[7].dayPassed(ss);
-            ss.finishedDrawing();
-        }
+        systemPlanets[7] = new Planet(ss, 420, 60190, 25, "BLUE");  
     }
-
 }
