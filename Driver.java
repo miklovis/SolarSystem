fc28f742e98634f850f5499c1bf4060b2e5faa6b
Arnas <a.miklovis@lancaster.ac.uk>
2020-10-13 09:51:20 +0100

    test

--- Driver.java
+++ Driver.java
@@ -1,14 +1,27 @@
 public class Driver {
+    static SolarSystem ss;
+    static final int earthToSun = 100;
+    static final int moonToEarth = 20;
+    
     public static void main(String[] args)
     {
-
-        SolarSystem ss = new SolarSystem(500, 500);
+        ss = new SolarSystem(500, 500);
         
-        ss.drawSolarObject(0, 0, 25, "YELLOW");
-
-        while(1 > 0){
-            ss.drawSolarObjectAbout(50, 350, 10, "BLUE", 50, 350);
-            ss.finishedDrawing();
+        while(true){
+            for(int i = 0; i < 360; i++){
+                ss.drawSolarObject(0, 0, 25, "YELLOW");
+                drawEarth(i);
+                drawMoon(i);
+                ss.finishedDrawing();
+            }
         }
+    }
+
+    public static void drawEarth(int angle){
+        ss.drawSolarObject(earthToSun, angle, 10, "BLUE");
+    }
+
+    public static void drawMoon(int angle){
+        ss.drawSolarObjectAbout(moonToEarth, angle, 5, "WHITE", moonToEarth, angle);
     }
 }
