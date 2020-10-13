4a80454fbcf7160674d7f41e0bd8702628d1fe71
Miklovis <miklovis@egb076000019.lancs.local>
2020-10-13 11:30:46 +0100

    Made class Planets usable and easily scalable

--- Driver.java
+++ Driver.java
@@ -1,28 +1,32 @@
 public class Driver {
     static SolarSystem ss;
-    static final int earthToSun = 100;
-    static final int moonToEarth = 20;
+    static Planet[] systemPlanets = new Planet[8];
     
     public static void main(String[] args)
     {
         ss = new SolarSystem(1000, 1000);
-        int j = 10;
+        systemPlanets[0] = new Planet(ss, 50, 88, 10, "WHITE");
+        systemPlanets[1] = new Planet(ss, 90, 225, 20, "YELLOW");
+        systemPlanets[2] = new Planet(ss, 140, 365, 25, "BLUE");
+        systemPlanets[3] = new Planet(ss, 180, 687, 20, "RED");
+        systemPlanets[4] = new Planet(ss, 250, 4333, 70, "ORANGE");
+        systemPlanets[5] = new Planet(ss, 310, 10759, 100, "YELLOW");
+        systemPlanets[6] = new Planet(ss, 380, 30687, 40, "CYAN");
+        systemPlanets[7] = new Planet(ss, 420, 60190, 25, "BLUE");        
         
         while(true){
-            for(int i = 0; i < 360; i++){
-                ss.drawSolarObject(0, 0, 25, "YELLOW");
-                drawEarth(i);
-                drawMoon(i);
-                ss.finishedDrawing();
-            }
+            System.out.println("Labas");
+            ss.drawSolarObject(0, 0, 50, "YELLOW");
+            systemPlanets[0].dayPassed(ss);
+            systemPlanets[1].dayPassed(ss);
+            systemPlanets[2].dayPassed(ss);
+            systemPlanets[3].dayPassed(ss);
+            systemPlanets[4].dayPassed(ss);
+            systemPlanets[5].dayPassed(ss);
+            systemPlanets[6].dayPassed(ss);
+            systemPlanets[7].dayPassed(ss);
+            ss.finishedDrawing();
         }
     }
 
-    public static void drawEarth(int angle){
-        ss.drawSolarObject(earthToSun, angle, 10, "BLUE");
-    }
-
-    public static void drawMoon(int angle){
-        ss.drawSolarObjectAbout(earthToSun, angle, 2, "WHITE", 10, angle * 4);
-    }
 }
--- Planet.java
+++ Planet.java
@@ -1,3 +1,45 @@
+import java.util.Random;
+
 public class Planet{
-    
+
+    Random rand = new Random();
+
+
+    private int distance;
+    private int daysPassed = 0;
+    private int daysToOrbitSun;
+    private int diameter;
+    private String col;
+
+
+    public Planet(SolarSystem solarSys, int distanceFromSun, int orbitLength, int diameter, String colour){
+        this.diameter = diameter;
+        daysToOrbitSun = orbitLength;
+        daysPassed = rand.nextInt(daysToOrbitSun);
+        col = colour;
+        distance = distanceFromSun;
+
+        solarSys.drawSolarObject(distanceFromSun, daysPassed, diameter, colour);
+    }
+
+    boolean didPlanetOrbit()
+    {
+        if(daysToOrbitSun == daysPassed){
+            return true;
+        }
+        else return false;
+    }
+
+    void dayPassed(SolarSystem solarSys)
+    {
+        if(didPlanetOrbit()){
+            daysPassed = 0;
+        }
+        else daysPassed++;
+
+        double daysPassedDbl = daysPassed;
+        double daysToOrbitSunDbl = daysToOrbitSun;
+
+        solarSys.drawSolarObject(distance, daysPassedDbl / daysToOrbitSunDbl * 360, diameter, col);
+    }
 }
